2c373a1bb629e424ba86929d1367adc5eefc26d1
ayushmaannsingh <sa776857@gmail.com>
2025-09-27 23:36:15 +0530

    EJS

---
+++ index.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const app = express();
+const path = require("path");
+
+const port = 8080;
+
+app.set("view engine", "ejs");
+app.set("views",  path.join(__dirname, "/views"));
+
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+    res.render("home");
+});
+
+app.get("/ig/:username", (req, res) => {
+    const followers = ["Ayush", "Ashu", "Suman", "Rajkumar"];
+    let {username} = req.params;
+res.render("instagram", {username , followers} );
+});
+
+app.get("/hello", (req, res) => {
+    res.send("hello ayush");
+});
+
+app.get("/rolldice", (req, res) => {
+    let diceVal = Math.floor(Math.random() * 10) + 1;
+    res.render("rolldice" , {diceVal});
+});
+
+app.listen(port, () => {
+    console.log(`listen on port ${port}`);
+    
+});
